e98de0e01a9438790106d322f66275d7ac6d2973
Mykola <mr.gigami@gmail.com>
2020-04-22 12:07:09 +0300

    finished lab 7

--- blog/src/app/components/blog-item-details/blog-item-details.component.ts
+++ blog/src/app/components/blog-item-details/blog-item-details.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {DataService} from '../../services/data.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'blog-item-details',
@@ -7,12 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogItemDetailsComponent implements OnInit {
 
-  image = 'http://osnews.pl/wp-content/uploads/2016/06/it-grafika.jpg';
-  text = 'Tytuł';
+  public image = '';
+  public text: string;
+  public id: number;
 
-  constructor() { }
+  constructor(private dataSerive: DataService , private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id: string;
+    this.route.paramMap
+      .subscribe(params => {
+        id = params.get('id');
+      });
+
+    this.dataSerive.getById(id).subscribe(res => {
+      this.image = res['image'];
+      this.text = res['text'];
+    });
   }
 
 }
--- blog/src/app/components/blog/blog.component.ts
+++ blog/src/app/components/blog/blog.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {DataService} from '../../services/data.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'blog',
